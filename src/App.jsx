05b6f785bdc30aabe5c2ff8b75367d236dde2365
pandarv <59077700+pandarv@users.noreply.github.com>
2022-07-30 23:59:30 -0600

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,26 @@ import data from "./data";
 import "./App.css";
 
 function App() {
-	const headerLogo = "../public/assets/logo.svg";
-	const cards = data.map((item, index) => {
-		if (index == data.length - 1) {
-			return <Card key={item.id} item={item} />;
-		} else if (data.length) {
-			return (
-				<div>
-					<Card key={item.id} item={item} />
-					<hr />
-				</div>
-			);
-		}
-	});
+    const headerLogo = "../public/assets/logo.svg";
+    const cards = data.map((item, index) => {
+        if (index == data.length - 1) {
+            return <Card key={item.id} item={item} />;
+        } else if (data.length) {
+            return (
+                <div>
+                    <Card key={item.id} item={item} />
+                    <hr />
+                </div>
+            );
+        }
+    });
 
-	return (
-		<div className="App">
-			<Header img={headerLogo} />
-			{cards}
-		</div>
-	);
+    return (
+        <div className="App">
+            <Header img={headerLogo} />
+            {cards}
+        </div>
+    );
 }
 
 export default App;
